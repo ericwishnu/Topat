10c352a373511c91726d8eaa371f84bfc261fdb6
Eric Wishnu Saputra <eric.wishnu@hotmail.com>
2013-03-24 15:22:58 +0700

    Copy

---
+++ src/parsingtable/Main.java
@@ -0,0 +1,20 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package parsingtable;
+
+/**
+ *
+ * @author wiznuq
+ */
+public class Main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+         GUI run = new GUI();
+        run.setVisible(true);
+    }
+}
