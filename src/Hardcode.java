61a77bfb5cf49d60c35604a8962612dce1bdcf6f
Shan Markus <shanmarkus@gmail.com>
2013-03-26 20:10:47 +0700

    java beans

--- src/Hardcode.java
+++ src/Hardcode.java
@@ -1,18 +1,73 @@
+
+import java.util.ArrayList;
+import javax.swing.JOptionPane;
+
 /*
  * To change this template, choose Tools | Templates
  * and open the template in the editor.
  */
-
 /**
  *
  * @author Shan
  */
 public class Hardcode {
 
+    ArrayList<String> nonterminal = new ArrayList<String>();
+    ArrayList<String> terminal = new ArrayList<String>();
+
+
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        String input = JOptionPane.showInputDialog("Please enter your input");
+        String [] a = checkInput(input);
+       
+       
+
+    }
+    
+    public static String[] checkInput(String input){        
+        String [] temp = input.split(" ");
+        return temp;
+    }
+    
+    public void check(String input){
+        for(int i = 0; i<terminal.size();i++){
+            if(input == terminal.get(i)){
+                
+            }
+        }
+        
+    }
+
+
+    private void Rules(String input) {
+        String temp = "E";
+        InititateRules();
+        String [] result = checkInput(input);
+        
+        if(result[0].equals("id")){
+            
+        }
+    }
+    
+    private void InititateRules(){
+        //nonterminal array
+        nonterminal.add("E");
+        nonterminal.add("E'");
+        nonterminal.add("T");
+        nonterminal.add("T'");
+        nonterminal.add("F");
+        
+        //terminal
+        terminal.add("(");
+        terminal.add("id");
+        terminal.add("+");
+        terminal.add("*");
+        terminal.add(")");
+        terminal.add("$");
+        
+        
     }
 }
